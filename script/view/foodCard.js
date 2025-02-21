f046c4e8bb54a9e6a685a06c0302ff643d1cc019
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-21 07:23:45 +0700

    Create image element from food object

--- script/view/foodCard.js
+++ script/view/foodCard.js
@@ -3,5 +3,10 @@ import createElement from "./createElement.js";
 export default function foodCard(foodArray) {
   foodArray.forEach((element) => {
     createElement(".content", "div", { id: `container-${element.id}` });
+    createElement(`#container-${element.id}`, "img", {
+      src: element.image.desktop,
+      alt: element.name,
+      class: `food-image`,
+    });
   });
 }
