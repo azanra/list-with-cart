85d3bef11fb96e298cc5a32513d792f6e9b3cd4d
azan-personal <112457939+azanra@users.noreply.github.com>
2025-02-28 15:42:46 +0700

    Extract addCartController into modules

--- script/controller/addCartController.js
+++ script/controller/addCartController.js
@@ -1,21 +1,37 @@
-import { deleteElement, getTheId } from "../util/util.js";
+import { util } from "../util/util.js";
 import { buttonActionCart } from "../view/addToCartBtn.js";
 
-export default function addToCartController() {
-  const toCartBtn = document.querySelectorAll(".toCartBtn");
-  toCartBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {
-      let parentId = btn.parentNode.id;
-      let onlyId = getTheId(parentId, 1);
-      deleteElement(`.btn-container-${onlyId}`);
-      buttonActionCart(onlyId);
+export const controller = {
+  putListener: function (element, event, listFood) {
+    const nodeElement = document.querySelectorAll(element);
+    nodeElement.forEach((item) => {
+      item.addEventListener("click", () => {
+        event(item, listFood);
+      });
     });
-  });
-}
+  },
+  addToCartEvent: function (element, listFood) {
+    let foodId = util.getParentId(element);
+    listFood.findFoodToEdit(foodId);
 
-function addActionListener(parentId) {
-  const increBtn = document.querySelectorAll(`incre-${parentId}`);
-  increBtn.forEach((btn) => {
-    btn.addEventListener("click", () => {});
-  });
-}
+    util.deleteElement(`.btn-container-${foodId}`);
+    util.deleteElement(`#btn-${foodId}`);
+    buttonActionCart(foodId);
+  },
+  addCartController: function (listFood) {
+    this.putListener(".toCartBtn", this.addToCartEvent, listFood);
+  },
+  incrementEvent: function (listFood) {
+    const increBtn = document.querySelectorAll(
+      `incre-${listFood.findEdittedFood()}`
+    );
+    if (increBtn !== null || increBtn !== undefined) {
+      increBtn.forEach((btn) => {
+        let foodId = util.getParentId(increBtn);
+        btn.addEventListener("click", () => {
+          listFood.incrementFoodAmount(foodId);
+        });
+      });
+    }
+  },
+};
