8d29f424acf269c08528879fdf337e6b08d58fd6
azan-personal <112457939+azanra@users.noreply.github.com>
2025-03-10 06:41:52 +0700

    Update total price in the modal order

--- script/view/orderItem.js
+++ script/view/orderItem.js
@@ -1,8 +1,10 @@
+import { util } from "../util/util.js";
 import { cartItem } from "./cartItem.js";
 
 export const orderItem = (function () {
   const extendCartItem = (listFood) => {
     cartItem.createItem(listFood, ".modal-cart", "modalSection");
+    util.setTextContent(".modal-amount", listFood.getTotalPrice());
   };
   return { extendCartItem };
 })();
