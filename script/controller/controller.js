1f2a15a698ed788ed13bcadd2be26c72c223f050
azan-personal <112457939+azanra@users.noreply.github.com>
2025-03-12 06:56:42 +0700

    Reset the amount, view to default on start order

--- script/controller/controller.js
+++ script/controller/controller.js
@@ -1,8 +1,10 @@
 import { foodController } from "./addCartController.js";
 import { confirmBtnController } from "./confirmBtnController.js";
+import { orderController } from "./orderController.js";
 
 export default function controller(listFood) {
   foodController.addCartController();
   foodController.actionController(listFood);
   confirmBtnController.confirmListener(listFood);
+  orderController.orderListener(listFood);
 }
--- script/controller/orderController.js
+++ script/controller/orderController.js
@@ -1,4 +1,6 @@
 import { util } from "../util/util.js";
+import foodCard from "../view/foodCard.js";
+import { foodController } from "./addCartController.js";
 
 export const orderController = (function () {
   const startOrder = document.querySelector("#start-order");
@@ -7,7 +9,17 @@ export const orderController = (function () {
     startOrder.addEventListener("click", () => {
       modalOrder.close();
       listFood.resetAllAmount();
+      resetView();
+      foodCard(listFood.getListFood());
+      foodController.addCartController();
+      foodController.actionController(listFood);
     });
+  };
+  const resetView = () => {
+    util.deleteElement("#contentContainer");
+    util.deleteElement(".sidebarOrder");
+    util.setTextContent(".order-amount", "");
+    util.deleteElement(".cartModalContainer");
   };
   return { orderListener };
 })();
